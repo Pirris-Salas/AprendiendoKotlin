acc6d5afedecc1db622f67f5784a04207e07e4b8
Pirris <luissalas_10@outlook.com>
2020-07-07 17:58:35 -0600

    Se agregan más operadores

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -1,4 +1,4 @@
-fun main(args: Array<String>) {
+fun main(args: Array<String>){
     println("Hola Mundo")
 
     val a = 4
@@ -14,5 +14,23 @@ fun main(args: Array<String>) {
     println(a.minus(b)) //resta
     println(a.times(b)) //multiplicación
     println(a.div(b)) // división
+
+    /**
+     * Devuelve un valor negativo y unaryPlus devuelve un valor positivo
+     */
+    var z = -2
+    var y = z.unaryMinus()
+    println("y: $y")
+
+    /**
+     * Valores boolean
+     */
+    var f = true
+    var s = f.not()
+    println("s: $s")
+
+    val t = -12
+    val r = 12
+    println("t es mayor que r. ${t>r}")
 
 }
