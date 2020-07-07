36276785942563b992f7a5acebe3f7a0a5e9babb
Pirris <luissalas_10@outlook.com>
2020-07-06 19:30:39 -0600

    Operadores básicos de Kotlin, suma - resta - multiplicación y división

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -1,3 +1,18 @@
 fun main(args: Array<String>) {
     println("Hola Mundo")
+
+    val a = 4
+    val b = 6
+
+    /**
+     * Aca asignamos operadores básicos de Kotlin. En Kotlin todo son objetos
+     * Por lo que podemos declarar variables y ver métodos como suma
+     * resta, multiplicación y división
+     */
+
+    println(a.plus(b)) //suma
+    println(a.minus(b)) //resta
+    println(a.times(b)) //multiplicación
+    println(a.div(b)) // división
+
 }
