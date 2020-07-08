31a7cd65cc1a1488671a67179b8d1e967c0d1264
Pirris <luissalas_10@outlook.com>
2020-07-07 20:11:09 -0600

    Se explica como utilizar variables tipo var, val y const

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -1,3 +1,14 @@
+const val N = "Name" // Const siempre se declara como variables globales y nunca como variables locales
+// Const se asigna en el proceso de compilación
+
+var n = "n" // Esto es una mala práctica ya que var es una variable changeable, por lo que va en contra de los principios
+// de la inmutabilidad en la programación funcional
+
+val j = "n" // Esto es válido, ya que val es una variable unchangeable, por lo que no va en contra de los pricipios de
+// la inmutabilidad. Val es se asigna en el proceso de ejecución del software. Val se utiliza más en variables locales
+// de los métodos y no en variables globales como Const
+
+//Funciones Puras
 fun main(args: Array<String>){
     println("Hola Mundo")
 
@@ -32,5 +43,17 @@ fun main(args: Array<String>){
     val t = -12
     val r = 12
     println("t es mayor que r. ${t>r}")
+
+    /**
+     * Esto es en tiempo de ejecución, ya que agregamos "Luis" como parámetro de args y el mismo es asignado hasta que se
+     * ejecuta el programa.
+     * Val: Tiempo de ejecución
+     * Const: Tiempo de compilación
+     */
+    val name:String = args[0]
+    println(name)
+
+    println(N) // N es declarado como variable global, y con un valor preasignado en tiempo de compilación
+
 
 }
